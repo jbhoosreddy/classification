1c97886a45ba2f130a77c6de7b9f2c6996f7de9d
Jaideep Bhoosreddy <jaideepb@buffalo.edu>
2016-12-09 15:28:21 -0500

    handle categorical data in decision trees

--- Boost.py
+++ Boost.py
@@ -9,7 +9,7 @@ __all__ = ['Boost']
 
 class Boost(object):
 
-    def __init__(self, T=5):
+    def __init__(self, N=5, **kwargs):
         self.allmodels = [KNN, NaiveBayes, RandomForest, DecisionTree]
         self.models = [choice(self.allmodels) for _ in choice(range(self.allmodels))]
         self.models = map(lambda m: m(), self.models)
